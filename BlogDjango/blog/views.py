78b7a0702ae28840c83c491974372f1f1874784c
Rui Reis <ruimartinseis@gmail.com>
2022-09-24 22:18:38 +0100

    Add Templates, Styling and Super user logins

--- BlogDjango/blog/views.py
+++ BlogDjango/blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+post =[
+    {
+        'author': 'Bro',
+        'title': 'Something',
+        'content':'First post',
+        'date_posted': '24/8/2022'
+    },
+    {
+        'author': 'Broette',
+        'title': 'Something',
+        'content':'First post',
+        'date_posted': '25/8/2022'
+    }
+]
+
+
+def home(request):
+    context = {'posts': post}
+    return render(request,'blog/home.html', context)
+
+def about(request):
+    return render(request,'blog/about.html', {'title': 'About'})
